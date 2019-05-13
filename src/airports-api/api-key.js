ab1d45dadff90fec19bffe844f3645d6ae957fb7
Sai Vikas Gopal <saivikas7875@gmail.com>
2019-05-12 20:12:03 -0400

    import material ui core and replace seed code

---
+++ src/airports-api/api-key.js
@@ -0,0 +1,3 @@
+export const AirportsApi = Object.freeze({
+    API_KEY: 'b3c93608-5e2d-4022-bbc5-9cbc47ff1273'
+});
---
+++ src/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+
+export const Header = () => (
+    <header className="app-header">
+        <AppBar color="primary">
+            <div>'Direct Flights'</div>
+        </AppBar>
+    </header>
+);
