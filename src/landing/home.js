702dde8aa6314ba6719db6f37ecc2faa3ecb051e
Sai Vikas Gopal <saivikas7875@gmail.com>
2019-05-31 03:56:53 -0400

    add card for dog breeds application

--- src/landing/home.js
+++ src/landing/home.js
@@ -1,10 +1,11 @@
 import './home.scss';
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link as RouterLink} from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 import {HomeHeader} from '../header/home-header';
 
 export const Home = () => {
@@ -15,7 +16,7 @@ export const Home = () => {
                 <Card classes={{root: 'home-card'}}>
                     <CardContent classes={{root: 'home-card-content'}}>
                         <Button
-                            component={Link}
+                            component={RouterLink}
                             to='/countries'
                             variant='text'
                             size='small'
@@ -31,7 +32,7 @@ export const Home = () => {
                 <Card classes={{root: 'home-card'}}>
                     <CardContent classes={{root: 'home-card-content'}}>
                         <Button
-                            component={Link}
+                            component={RouterLink}
                             to='/capital-quiz'
                             variant='text'
                             size='small'
@@ -41,6 +42,22 @@ export const Home = () => {
                         </Button>
                         <Typography variant='body1' classes={{root: 'home-card-info'}}>
                             Test your knowledge about the world's capital cities
+                        </Typography>
+                    </CardContent>
+                </Card>
+                <Card classes={{root: 'home-card'}}>
+                    <CardContent classes={{root: 'home-card-content'}}>
+                        <Link
+                            href='https://dog-breeds.saivikas2804.now.sh/'
+                            classes={{root: 'link-root link-label third-party-link'}}
+                            target='_blank'
+                            underline='none'
+                            rel='noopener noreferrer'
+                        >
+                            Dog breeds
+                        </Link>
+                        <Typography variant='body1' classes={{root: 'home-card-info'}}>
+                            Get random images of dogs based on their breeds. Familiarize yourself with different breeds.
                         </Typography>
                     </CardContent>
                 </Card>
